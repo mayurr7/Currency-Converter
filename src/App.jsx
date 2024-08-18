ce5137e978cc3471d9698b3dd70b93b424b654d7
Mayur Tekale <mayurtekale7@gmail.com>
2024-08-18 20:43:58 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-
+import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>mayur</h1>
+      <h1 className='text-3xl bg-red-600'>mayur 1</h1>
     </>
   )
 }
