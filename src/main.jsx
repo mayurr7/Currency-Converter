1e3a20caadac93cf59ab1911ed33a7d3d9063b93
Mayur Tekale <mayurtekale7@gmail.com>
2024-09-09 17:44:22 +0530

    done UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import InputBox from './components/InputBox.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+    <InputBox />
+    
+  </React.StrictMode>,
 )
